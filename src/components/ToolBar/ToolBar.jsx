baa865de7a7a7f2f968fad259dbe64188988a47b
MahshadOliaie <mahshadoliaie@gmail.com>
2024-04-24 18:54:06 +0330

    add products page btn

--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -5,6 +5,7 @@ import FavoriteItems from '../../context/favoriteItem/FavoriteItem';
 import { useNavigate } from 'react-router-dom';
 import HomeSvg from '../SVGs/HomeSvg/HomeSvg';
 import CartSvg from '../SVGs/CartSvg/CartSvg';
+import ShoeSvg from '../SVGs/ShoeSvg/ShoeSvg';
 
 function ToolBar() {
     const navigate = useNavigate()
@@ -13,8 +14,8 @@ function ToolBar() {
     return (
         <div className={CSS.toolBar}>
             <HomeSvg />
+            <ShoeSvg />
             <CartSvg />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z" /></svg>
             <HeartSvg hasLabel={true} label={favoriteItems.length} onClick={() => { navigate(`/favorite`) }} />
         </div>
     )
